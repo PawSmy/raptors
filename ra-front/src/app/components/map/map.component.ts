f358933c4c880d737ced910dbd4af52e38b21738
Pawel <pawelsmyczynski@tlen.pl>
2020-11-17 09:17:44 +0100

    Dynamic task panel

--- ra-front/src/app/components/map/map.component.ts
+++ ra-front/src/app/components/map/map.component.ts
@@ -148,13 +148,16 @@ export class MapComponent implements OnInit, OnDestroy {
         this.mapOriginY = mapData.mapOriginY;
         this.mapService.getMap(this.mapId).subscribe(
           data => {
-            this.afterMapLoaded(data);
-            this.subscribe = this.source.subscribe(val => {
-              this.robotService.getAll().subscribe(
-              robots=>{
-                this.updateRobots(robots);
+            const container = document.getElementById('map');
+            if(container) {
+              this.afterMapLoaded(data);
+              this.subscribe = this.source.subscribe(val => {
+                this.robotService.getAll().subscribe(
+                robots=>{
+                  this.updateRobots(robots);
+                });
               });
-            });
+            }
           }
         );
       }
@@ -166,7 +169,11 @@ export class MapComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscribe.unsubscribe();
+    try {
+      this.subscribe.unsubscribe();      
+    } catch (error) {
+      
+    }
     //this.subscription.unsubscribe();
   }
 
